76a6cc34cdade7c6757f053003697427c989e9a0
Sergey Glukhov <sergey.glukhov@gmail.com>
2018-01-24 17:09:51 +0300

    localize all texts

--- app/src/main/java/com/anjlab/btdriving/app/DetectedActivitiesIntentService.kt
+++ app/src/main/java/com/anjlab/btdriving/app/DetectedActivitiesIntentService.kt
@@ -31,8 +31,8 @@ class DetectedActivitiesIntentService : IntentService(TAG) {
             if (pref.debug) {
                 Notifications.createNotification(
                     this,
-                    getActivityString(activity.type),
-                    "confidence: ${activity.confidence}",
+                    resources.getStringArray(R.array.activity_kinds)[activity.type],
+                    "${getString(R.string.activity_confidence)}: ${activity.confidence}",
                     3
                 )
             }
@@ -57,19 +57,5 @@ class DetectedActivitiesIntentService : IntentService(TAG) {
 
     companion object {
         private const val TAG = "DetectedActivitiesIntentService"
-
-        internal fun getActivityString(detectedActivityType: Int): String {
-            return when (detectedActivityType) {
-                DetectedActivity.IN_VEHICLE -> "in_vehicle"
-                DetectedActivity.ON_BICYCLE -> "on_bicycle"
-                DetectedActivity.ON_FOOT -> "on_foot"
-                DetectedActivity.RUNNING -> "running"
-                DetectedActivity.STILL -> "still"
-                DetectedActivity.TILTING -> "tilting"
-                DetectedActivity.UNKNOWN -> "unknown"
-                DetectedActivity.WALKING -> "walking"
-                else -> "unidentifiable_activity"
-            }
-        }
     }
 }
